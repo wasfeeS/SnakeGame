fe4991cf0befd0cfbd907d496e07984ad34469c6
Wasfee <Wasfee@DESKTOP-F9MJ5P4.home>
2024-04-18 21:44:32 -0400

    continued working on panel

--- src/snakeGame/gamePanel.java
+++ src/snakeGame/gamePanel.java
@@ -6,22 +6,23 @@ package snakeGame;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.Random;
-import javax.swing.Timer;
+import javax.swing.*;
+
 /**
  *
  * @author Wasfee
  */
-public class gamePanel extends javax.swing.JPanel implements ActionListener{
+public class gamePanel extends JPanel implements ActionListener{
     static final int SCREEN_WIDTH = 600;
     static final int SCREEN_HEIGHT = 600;
-    static final int UNIT_SIZE = 25;
+    static final int UNIT_SIZE = 50;
     static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
     static final int DELAY = 75;
     final int x[] = new int[GAME_UNITS];
     final int y[] = new int[GAME_UNITS];
     int bodyParts = 6;
     int applesEaten;
-    int leX;
+    int appleX;
     int appleY;
     char direction = 'R';
     boolean running = false;
@@ -37,23 +38,61 @@ public class gamePanel extends javax.swing.JPanel implements ActionListener{
         initComponents();
     }
     public void startGame(){
-        
+        newApple();
+        running = true;
+        timer = new Timer(DELAY, this);
+        timer.start();
     }   
     
     public void paintCOmponent(Graphics g){
-        
+        super.paintComponent(g);
     }
     
     public void draw(Graphics g){
+        for(int i = 0; i>SCREEN_HEIGHT/UNIT_SIZE;i++){
+            g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+            g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
+        }
+        g.setColor(Color.red);
+        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
         
+        for(int i = 0; i<bodyParts; i++){
+        if(i==0){
+            g.setColor(Color.green);
+            g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+        }
+        else{
+            g.setColor(new Color(45, 180, 0));
+            g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+        }
+    }
     }
     
     public void newApple(){
-        
+        appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE));
+        appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE));
     }
     
     public void move(){
-        
+        for(int i = bodyParts; i>0; i--){
+            x[i] = x[i-1];
+            y[i] = y[i-1];   
+        }
+        switch(direction){
+            case 'U':
+                y[0] = y[0]-UNIT_SIZE;
+                break;
+            case 'D':
+                y[0] = y[0]+UNIT_SIZE;
+                break;
+            case 'L':
+                x[0] = x[0]-UNIT_SIZE;
+                break;
+            case 'r':
+                x[0] = x[0]+UNIT_SIZE;
+                break;
+
+        }
     }
     
     public void checkApple(){
@@ -72,6 +111,8 @@ public class gamePanel extends javax.swing.JPanel implements ActionListener{
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        setBackground(new java.awt.Color(0, 0, 0));
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
@@ -86,7 +127,12 @@ public class gamePanel extends javax.swing.JPanel implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        if(running){
+            move();
+            checkApple();
+            checkCollisions();
+        }
+        repaint();
     }
     
     public class MyKeyAdapter extends KeyAdapter{
